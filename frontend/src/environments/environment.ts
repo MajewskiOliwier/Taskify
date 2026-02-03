f73c16a8d32e8bef37292d37c9e0b86545980637
Carlo <galante.carl@gmail.com>
2026-02-03 18:49:50 +0100

    switched to nginx

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://fp7qhaupmd.execute-api.us-east-1.amazonaws.com'  // API Gateway AWS
+  apiUrl: 'http://localhost:3000' // local dev
 };
 
-// apiUrl: 'http://localhost:3000' // local dev
+//   apiUrl: 'https://fp7qhaupmd.execute-api.us-east-1.amazonaws.com'  // API Gateway AWS
