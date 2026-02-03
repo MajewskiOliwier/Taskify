6089648ac224f4286756f7988bf6501004e4b5ca
Carlo <galante.carl@gmail.com>
2026-02-03 08:22:32 +0100

    changed again to vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4200,
+    allowedHosts: [
+      'localhost',
+      'test-taskify-frontend-lb-2028155531.us-east-1.elb.amazonaws.com',
+      '.elb.amazonaws.com'
+    ]
+  }
+});
